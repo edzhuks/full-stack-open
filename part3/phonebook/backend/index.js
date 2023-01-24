fd53f1d620cb12b61579df9527e7a367634985ae
edgars <edzhuks@gmail.com>
2023-01-24 22:38:01 +0200

    3b

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -3,6 +3,7 @@ const {response} = require("express");
 const morgan = require("morgan")
 const app = express()
 
+app.use(express.static("build"))
 app.use(express.json())
 morgan.token("postbody", function (req, res) { return req.method==="POST"?JSON.stringify(req.body):" "})
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :postbody"))
