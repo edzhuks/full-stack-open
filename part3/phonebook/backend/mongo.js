b74abf8b9bb04362bbc5d9a8f85d282569de33fd
edgars <edzhuks@gmail.com>
2023-02-13 23:05:09 +0200

    3d

--- part3/phonebook/backend/mongo.js
+++ part3/phonebook/backend/mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length<3) {
-    console.log('give password as argument')
-    process.exit(1)
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -14,31 +14,31 @@ mongoose.set('strictQuery',false)
 mongoose.connect(url)
 
 const entriesSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+  name: String,
+  number: String,
 })
 
 const Entry = mongoose.model('Entry', entriesSchema)
 
 if (process.argv.length > 3) {
-    const name = process.argv[3]
-    const number = process.argv[4]
+  const name = process.argv[3]
+  const number = process.argv[4]
 
-    const entry = new Entry({
-        name: name,
-        number: number,
-    })
+  const entry = new Entry({
+    name: name,
+    number: number,
+  })
 
-    entry.save().then(result => {
-        console.log(`added ${name} number ${number} to phonebook`)
-        mongoose.connection.close()
-    })
+  entry.save().then(() => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
 }
 else {
-    Entry.find({}).then(result => {
-        result.forEach(note => {
-            console.log(note)
-        })
-        mongoose.connection.close()
+  Entry.find({}).then(result => {
+    result.forEach(note => {
+      console.log(note)
     })
+    mongoose.connection.close()
+  })
 }
