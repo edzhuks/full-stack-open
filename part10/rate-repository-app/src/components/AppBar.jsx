949edd5a9ae79070c30788670968891de92d7503
edgars <edzhuks@gmail.com>
2023-03-30 21:50:11 +0300

    part10-ex4

---
+++ part10/rate-repository-app/src/components/AppBar.jsx
@@ -0,0 +1,37 @@
+import { View, StyleSheet, Pressable } from 'react-native';
+import Constants from 'expo-constants';
+import Text from './Text';
+import theme from '../theme';
+
+const styles = StyleSheet.create({
+    container: {
+        paddingTop: Constants.statusBarHeight,
+        backgroundColor: theme.colors.appBarBackground,
+    },
+    text: {
+        color: theme.colors.appBarText,
+        fontWeight: theme.fontWeights.bold,
+        fontSize: theme.fontSizes.appBar,
+    },
+    tab: {
+        padding: theme.paddings.appBarTab,
+    },
+});
+
+const AppBarTab = ({ text }) => {
+    return (
+        <Pressable style={styles.tab}>
+            <Text style={styles.text}>{text}</Text>
+        </Pressable>
+    );
+};
+
+const AppBar = () => {
+    return (
+        <View style={styles.container}>
+            <AppBarTab text="Repositories" />
+        </View>
+    );
+};
+
+export default AppBar;
---
+++ part10/rate-repository-app/src/theme.js
@@ -0,0 +1,26 @@
+const theme = {
+    colors: {
+        appBarBackground: '#24292e',
+        appBarText: 'white',
+        textPrimary: '#24292e',
+        textSecondary: '#586069',
+        primary: '#0366d6',
+    },
+    fontSizes: {
+        body: 14,
+        subheading: 16,
+        appBar: 18,
+    },
+    fonts: {
+        main: 'System',
+    },
+    fontWeights: {
+        normal: '400',
+        bold: '700',
+    },
+    paddings: {
+        appBarTab: 20,
+    },
+};
+
+export default theme;
